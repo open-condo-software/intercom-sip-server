aa2ededf8403a26386bf506b56d1c65323c5a364
Fora Soft <team@fora-soft.com>
2024-04-04 18:29:07 +0300

    Fix codecs

--- services/frontend/src/utils/removeCodec.ts
+++ services/frontend/src/utils/removeCodec.ts
@@ -1,22 +1,26 @@
 export const removeCodec = (sdp: string, codecToRemove = 'h264'): string => {
-  const lines = [];
-  const codecRegex = new RegExp(`/^a=rtpmap:(\d+) ${codecToRemove}/`, 'i');
-  let mline = -1;
-  let rtpRegex: RegExp | null = null;
-  let rtpId = '';
-  for (const line of sdp.split('\n')) {
-    const match = line.match(codecRegex);
-    if (line.startsWith('m=video')) {
-      mline = lines.push(line) - 1;
-    } else if (match) {
-      rtpId = match[1];
-      rtpRegex = new RegExp(`^a=(rtpmap|rtcp-fb|fmtp):${rtpId}`);
-    } else if (!rtpRegex || !line.match(rtpRegex)) {
-      lines.push(line);
+  const codecRegex = new RegExp(`^a=rtpmap:(\\d+) ${codecToRemove}`, 'i');
+  do {
+    const lines = [];
+    let mline = -1;
+    let rtpRegex: RegExp | null = null;
+    let rtpId = '';
+    for (const line of sdp.split('\n')) {
+      const match = line.match(codecRegex);
+      if (line.startsWith('m=video')) {
+        mline = lines.push(line) - 1;
+      } else if (match) {
+        rtpId = match[1];
+        rtpRegex = new RegExp(`^a=(rtpmap|rtcp-fb|fmtp):${rtpId}`);
+      } else if (!rtpRegex || !line.match(rtpRegex)) {
+        lines.push(line);
+      }
     }
-  }
-  if (mline >= 0 && rtpId) {
-    lines[mline] = lines[mline].replace(' ' + rtpId, '');
-  }
-  return lines.join('\n');
+    if (mline >= 0 && rtpId) {
+      lines[mline] = lines[mline].replace(' ' + rtpId, '');
+      sdp = lines.join('\n');
+    } else {
+      return lines.join('\n');
+    }
+  } while (true);
 };
